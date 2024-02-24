0515757f39c71e70b5959bd4843be18dc8976832
Kingkaspev <kingsleychiboy22@gmail.com>
2024-02-24 12:41:57 +0100

    Unit Testing

--- MovieSearch.Api/MovieSearch/Program.cs
+++ MovieSearch.Api/MovieSearch/Program.cs
@@ -11,8 +11,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .WithExposedHeaders("Authorization");
+    });
+});
+
 
 var app = builder.Build();
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ MovieSearch.Api/MovieSearchTesting/ServiceImplementationTests/MovieServiceTests.cs
@@ -0,0 +1,99 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
+using MovieSearch.Application.ServiceImplementation;
+using MovieSearch.Domain;
+using MovieSearch.Domain.Entities;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace MovieSearchTesting.ServiceImplementation
+{
+    public class MovieServiceTests
+    {
+        [Fact]
+        public async Task SearchMovieByTitleAsync_ShouldReturnSuccessResponse()
+        {
+            // Arrange
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            var httpClientMock = new Mock<HttpClient>();
+            httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClientMock.Object);
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+
+            var appSettings = configuration.GetSection("AppSettings").Get<AppSettings>();
+            var appSettingsMock = new Mock<IOptions<AppSettings>>();
+            appSettingsMock.Setup(_ => _.Value).Returns(appSettings);
+
+            var movieService = new MovieService(httpClientFactoryMock.Object, appSettingsMock.Object);
+
+            var fakeApiResponse = ApiResponse<IEnumerable<MovieResult>>.Success(new List<MovieResult>
+            {
+                new MovieResult { Title = "Fake Movie 1" },
+                new MovieResult { Title = "Fake Movie 2" }
+            }, "Fake success", 200);
+
+            var fakeHttpContent = new StringContent(JsonConvert.SerializeObject(fakeApiResponse));
+            var fakeHttpResponse = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = fakeHttpContent
+            };
+
+            httpClientMock.Setup(c => c.GetAsync(It.IsAny<string>())).ReturnsAsync(fakeHttpResponse);
+
+            // Act
+            var result = await movieService.SearchMovieByTitleAsync("FakeTitle");
+
+            // Assert
+            Assert.True(result.Succeeded);
+            Assert.Equal("Fake success", result.Message);
+            Assert.Equal(200, result.StatusCode);
+
+            // Additional assertion to check the content
+            var movieResults = Assert.IsAssignableFrom<IEnumerable<MovieResult>>(result.Data);
+            Assert.Equal(2, movieResults.Count()); // Adjust the count based on your expected data
+            Assert.Contains(movieResults, m => m.Title == "Fake Movie 1");
+            Assert.Contains(movieResults, m => m.Title == "Fake Movie 2");
+        }
+
+        [Fact]
+        public async Task SearchMovieByTitleAsync_ShouldReturnNotFoundResponseOnApi404()
+        {
+            // Arrange
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            var httpClientMock = new Mock<HttpClient>();
+            httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClientMock.Object);
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var appSettings = new AppSettings();
+            configuration.GetSection("AppSettings").Bind(appSettings);
+            var appSettingsMock = new Mock<IOptions<AppSettings>>();
+            appSettingsMock.Setup(_ => _.Value).Returns(appSettings);
+
+            var movieService = new MovieService(httpClientFactoryMock.Object, appSettingsMock.Object);
+
+            var fakeHttpResponse = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                Content = new StringContent("Not Found")
+            };
+
+            httpClientMock.Setup(c => c.GetAsync(It.IsAny<string>())).ReturnsAsync(fakeHttpResponse);
+
+            // Act
+            var result = await movieService.SearchMovieByTitleAsync("NonExistentTitle");
+
+            // Assert
+            Assert.False(result.Succeeded);
+            Assert.Equal("No results found", result.Message);
+            Assert.Equal(404, result.StatusCode);
+        }
+    }
+}
